363e1aa8c3bafb2e4337a747865f6c42bdc9e19c
Atrii27 <162042497+Atrii27@users.noreply.github.com>
2025-10-10 04:28:47 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,31 +1,41 @@
-import tensorflow as tf
-from tensorflow.keras import layers, Model
-def res_block(x, filters):
-    shortcut = layers.Conv2D(filters, 1, padding="same")(x)
-    y = layers.Conv2D(filters, 3, padding="same", activation="relu")(x)
-    y = layers.Conv2D(filters, 3, padding="same")(y)
-    y = layers.Add()([shortcut, y])
-    y = layers.Activation("relu")(y)
-    return y
-def encoder_block(x, filters):
-    y = res_block(x, filters)
-    p = layers.MaxPool2D()(y)
-    return y, p
-def decoder_block(x, skip, filters):
-    x = layers.UpSampling2D()(x)
-    x = layers.Concatenate()([x, skip])
-    x = res_block(x, filters)
+from tensorflow.keras import layers, models
+def conv_block(x, filters):
+    shortcut = x
+    x = layers.Conv2D(filters, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.Conv2D(filters, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    shortcut = layers.Conv2D(filters, 1, padding="same")(shortcut)
+    shortcut = layers.BatchNormalization()(shortcut)
+    x = layers.Add()([x, shortcut])
+    x = layers.Activation("relu")(x)
     return x
-def resunet_model(input_shape=(128,128,4)):
+def encoder_block(x, filters):
+    f = conv_block(x, filters)
+    p = layers.MaxPooling2D((2, 2))(f)
+    return f, p
+def decoder_block(x, skip, filters):
+    x = layers.Conv2DTranspose(filters, (2, 2), strides=2, padding="same")(x)
+    x = layers.Concatenate()([x, skip])
+    x = conv_block(x, filters)
+    return x
+def build_resunet(input_shape=(128, 128, 4)):
     inputs = layers.Input(shape=input_shape)
-    c1, p1 = encoder_block(inputs, 64)
-    c2, p2 = encoder_block(p1, 128)
-    c3, p3 = encoder_block(p2, 256)
-    c4, p4 = encoder_block(p3, 512)
-    b = res_block(p4, 1024)
-    d1 = decoder_block(b, c4, 512)
-    d2 = decoder_block(d1, c3, 256)
-    d3 = decoder_block(d2, c2, 128)
-    d4 = decoder_block(d3, c1, 64)
+    # Encoder
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+    # Bridge
+    b = conv_block(p4, 1024)
+    # Decoder
+    d1 = decoder_block(b, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
     outputs = layers.Conv2D(1, 1, activation="sigmoid")(d4)
-    return Model(inputs, outputs)
+    model = models.Model(inputs, outputs, name="ResU-Net")
+    return model
+
+
